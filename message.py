cd67ee764dd2e0391679763f188cd781e8f28a9d
yuyunwuuu <lori78216@gmail.com>
2024-05-28 13:05:02 +0800

    Update message.py

--- message.py
+++ message.py
@@ -6,7 +6,7 @@ from linebot.models import *
 #ImagemapSendMessage(組圖訊息)
 def imagemap_message():
     message = ImagemapSendMessage(
-        base_url="https://ibb.co/7vSF816",
+        base_url="https://imgur.com/a/K1QlZEE",
         alt_text='最新的合作廠商有誰呢？',
         base_size=BaseSize(height=2000, width=2000),
         actions=[
@@ -170,28 +170,28 @@ def image_carousel_message1():
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/uKYgfVs.jpg",
                     action=URITemplateAction(
-                        label="新鮮水果",
+                        label="橘黃",
                         uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
                     )
                 ),
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/QOcAvjt.jpg",
                     action=URITemplateAction(
-                        label="新鮮蔬菜",
+                        label="橘紅",
                         uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
                     )
                 ),
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/Np7eFyj.jpg",
                     action=URITemplateAction(
-                        label="可愛狗狗",
+                        label="褐紅",
                         uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
                     )
                 ),
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/QRIa5Dz.jpg",
                     action=URITemplateAction(
-                        label="可愛貓咪",
+                        label="深褐",
                         uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
                     )
                 )
