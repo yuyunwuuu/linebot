61051edae1e564ba5dc4f055c6b1ad9fc8c528bf
yuyunwuuu <lori78216@gmail.com>
2024-05-29 19:02:34 +0800

    Update new.py

--- new.py
+++ new.py
@@ -9,30 +9,30 @@ def test():
         template=ImageCarouselTemplate(
             columns=[
                 ImageCarouselColumn(
-                    image_url="https://imgur.com/NkOCTRl",
+                    image_url="https://i.imgur.com/NkOCTRl.jpg",
                     action=URITemplateAction(
                         label="橘黃",
                         uri="https://plus.google.com/116237864387312784020/posts/S327kJKy6HG"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://i.imgur.com/QOcAvjt.jpg",
+                    image_url="https://i.imgur.com/tzpIEbX.jpg",
                     action=URITemplateAction(
                         label="橘紅",
                         uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://i.imgur.com/Np7eFyj.jpg",
+                    image_url="https://i.imgur.com/QXqpjZv.jpg",
                     action=URITemplateAction(
-                        label="可愛狗狗",
+                        label="紅褐",
                         uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
+                    image_url="https://i.imgur.com/ZL8mXUd.jpg",
                     action=URITemplateAction(
-                        label="可愛貓咪",
+                        label="深褐",
                         uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
                     )
                 )
